492a1fb9e0162d4cf106adf356e845735f46b29a
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-06-06 23:01:38 +0200

    added file storage and edited migrations

--- resources/js/pages/AboutUs.vue
+++ resources/js/pages/AboutUs.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        About us
+        <img
+            class="img-fluid"
+            src="https://best5.it/b5/wp-content/uploads/2018/07/La-storia-dello-zio-Sam2-800x400.jpg"
+            alt="zio sem want u"
+        />
     </div>
 </template>
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        Home
+    <div class="">
+        <img
+            class="img-fluid"
+            src="https://www.winemeridian.com/panel/lib/timthumb.php?src=/upload/articoli/2896/big/questa-casa-non-albergo.jpg&w=738&h=428&zc=2"
+            alt=" questa casa non e'una albergo"
+        />
     </div>
 </template>
 
